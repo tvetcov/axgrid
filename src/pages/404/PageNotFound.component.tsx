99516279a07028e295c15ddfe3f13d7f4b2b222d
Nichita Tvetcov <nt@encode.dk>
2024-02-27 16:44:05 +0200

    Added 404 page styles

--- src/pages/404/PageNotFound.component.tsx
+++ src/pages/404/PageNotFound.component.tsx
@@ -1,9 +1,34 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import UndoIcon from '@mui/icons-material/Undo';
+
 import ContentWrapper from 'components/ContentWrapper';
+import useNavigateTo from 'hooks/useNavigateTo.hook.ts';
+import { ROUTE_LIST } from 'pages/routes.ts';
+import pageNotFoundStyles from './pageNotFound.styles.ts';
 
 const PageNotFound = () => {
+    const navigate = useNavigateTo();
+
+    const handleGoBack = () => {
+        navigate(ROUTE_LIST.home);
+    };
+
     return (
         <ContentWrapper>
-            <div>404: Page Not Found</div>
+            <Box sx={pageNotFoundStyles}>
+                <Typography variant="h1" mb={4}>
+                    404: Page Not Found
+                </Typography>
+                <Button
+                    onClick={handleGoBack}
+                    endIcon={<UndoIcon />}
+                    variant="contained"
+                >
+                    Go Home
+                </Button>
+            </Box>
         </ContentWrapper>
     );
 };
---
+++ src/pages/404/pageNotFound.styles.ts
@@ -0,0 +1,8 @@
+export default {
+    height: '100%',
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+};
