1b0bc9f2d0231b2bfd5a747824e502c08d72af29
Nichita Tvetcov <nt@encode.dk>
2024-02-28 11:43:51 +0200

    Added base to vite ocnfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    base: '/axgrid/',
     plugins: [react(), TanStackRouterVite(), tsconfigPaths()]
 });
