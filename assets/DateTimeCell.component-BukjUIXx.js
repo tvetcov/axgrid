40601664ec9713eb762e90921dc21e5749aa84f2
Nichita Tvetcov <nt@encode.dk>
2024-02-29 22:19:48 +0200

    Updates

---
+++ assets/DateTimeCell.component-BukjUIXx.js
@@ -0,0 +1,1 @@
+import{S as a,j as t}from"./index-Bz09VXd3.js";import{e as o}from"./dashboard.index.lazy-DXuGnwAL.js";import"./Toolbar-cYpJcjRu.js";const i=({value:e})=>{const r=a(new Date(e),o);return t.jsxs(t.Fragment,{children:[t.jsx("b",{children:"Until:"})," ",r]})};export{i as default};
